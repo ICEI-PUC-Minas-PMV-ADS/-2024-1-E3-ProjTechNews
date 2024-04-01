41165f84f972469a983e49447b99218d13979e10
Pedroaguilar1232 <pedroaguilar1232@gmail.com>
2024-04-01 20:44:35 -0300

    update axios

--- tech-news/src/lib/axios.ts
+++ tech-news/src/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'http://192.168.0.5:3001', // http://localhost:3001 substituir localhost pelo ip local
 });
 
 export default instance;
