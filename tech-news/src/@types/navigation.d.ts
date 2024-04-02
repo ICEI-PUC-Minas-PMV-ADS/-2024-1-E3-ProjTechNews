8b38a3cd7209cd5a66d42289850c7e019639f6b4
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2024-04-02 01:53:50 -0300

    create @types

---
+++ tech-news/src/@types/navigation.d.ts
@@ -0,0 +1,10 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      login: undefined;
+      signUp: undefined;
+      home: undefined;
+      addNew: undefined;
+    }
+  }
+}
