0ce111d7ee9f1bd8313b2ecf4ff900b2c9c1d8d5
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2024-04-03 19:43:18 -0300

    update screen home

--- tech-news/src/components/screens/Home/index.tsx
+++ tech-news/src/components/screens/Home/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import NewsCard from '../../NewsCard';
 import { Footer, HomeContainer } from './styles';
 import Header from '../../Header';
+import { ScrollView } from 'react-native';
+import NewsCard from '../../NewsCard';
 
 const Home = () => {
   return (
     <HomeContainer>
-      <Header />
-      <NewsCard />
-      <Footer>puc minas</Footer>
+      {/* Usar por enquanto o ScrollView */}
+      <ScrollView> 
+        <Header />
+        <NewsCard />
+        <Footer>puc minas</Footer>
+      </ScrollView>
     </HomeContainer>
   );
 };
