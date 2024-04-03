056f42ac66a67d19731f8e065127f315aea1d600
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2024-04-03 19:43:41 -0300

    update screen login

--- tech-news/src/components/screens/Login/index.tsx
+++ tech-news/src/components/screens/Login/index.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
-import { LoginContainer, PwdInput, UserInput } from './styles';
+import { LoginContainer, LoginContent } from './styles';
 import Header from '../../Header';
+import Button from '../../Button';
+import Input from '../../Input';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 const Login = () => {
+  const route = useRoute();
+  const navigation = useNavigation();
+
+  const handleToSignUpPage = () => {
+    if (route.name === 'signUp') return;
+
+    navigation.navigate('signUp');
+  };
+
+  const handleLogin = () => {
+    console.log('login');
+  };
+
   return (
     <LoginContainer>
       <Header />
-      <UserInput placeholder={'Digite seu e-mail'} />
-      <PwdInput placeholder={'Digite sua senha'} />
+      <Router>
+        <LoginContent>
+          <Input placeholder="Digite seu e-mail" />
+          <Input placeholder="Digite sua senha" />
+
+          <Button
+            title="Login"
+            style={{ marginTop: 12 }}
+            onPress={handleLogin}
+          />
+
+          <Button
+            title="Cadastrar"
+            style={{ marginTop: 12 }}
+            type="SECONDARY"
+            onPress={handleToSignUpPage}
+          />
+        </LoginContent>
+      </Router>
     </LoginContainer>
   );
 };
--- tech-news/src/components/screens/Login/styles.ts
+++ tech-news/src/components/screens/Login/styles.ts
@@ -1,9 +1,15 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
-export const LoginContainer = styled.View`
-  width: 100%;
+export const LoginContainer = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  padding: 24px;
 `;
 
-export const UserInput = styled.TextInput``;
-
-export const PwdInput = styled.TextInput``;
+export const LoginContent = styled.View`
+  flex: 1;
+  /* display: flex; */
+  justify-content: center;
+  align-items: center;
+`;
