392ab38f6335091126f05cf09c17469b77ed285e
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2024-04-15 21:00:16 -0300

    update Home screen

--- tech-news/src/components/screens/Home/index.tsx
+++ tech-news/src/components/screens/Home/index.tsx
@@ -1,18 +1,75 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { Footer, HomeContainer } from './styles';
 import Header from '../../Header';
-import { ScrollView } from 'react-native';
+import { Alert, FlatList } from 'react-native';
 import NewsCard from '../../NewsCard';
+import { useFocusEffect, useNavigation, useRoute } from '@react-navigation/native';
+import instance from '../../../lib/axios';
+import Button from '../../Button';
+
+type User = {
+  id: Number;
+  name: string;
+  email: string;
+};
+
+type News = {
+  id: Number;
+  title: string;
+  url: string;
+  createdAt: string;
+  user: User;
+};
 
 const Home = () => {
+  const [news, setNews] = useState<News[]>([]);
+
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const handleToAddNewsPage = () => {
+    if (route.name === 'addNews') return;
+
+    navigation.navigate('addNews');
+  };
+
+  const fetchNews = useCallback(async () => {
+    try {
+      const { data } = await instance.get('/news');
+
+      setNews(data);
+    } catch (error) {
+      Alert.alert('Erro ⚠', 'Não foi possível carregar as notícias.');
+    }
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchNews();
+    }, [fetchNews])
+  );
+
   return (
     <HomeContainer>
-      {/* Usar por enquanto o ScrollView */}
-      <ScrollView> 
-        <Header />
-        <NewsCard />
-        <Footer>puc minas</Footer>
-      </ScrollView>
+      <Header />
+      <Button
+        title="Adicionar Notícias"
+        style={{ marginTop: 12 }}
+        onPress={handleToAddNewsPage}
+      />
+      
+      <FlatList
+        data={news}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <NewsCard
+            title={item.title}
+            url={item.url}
+            author={item.user?.name}
+          />
+        )}
+      />
+      <Footer>puc minas</Footer>
     </HomeContainer>
   );
 };
