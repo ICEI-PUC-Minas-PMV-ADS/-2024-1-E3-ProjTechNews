6cb17c151ef772ea2beb1a5c4cbf75ad70fc9ba3
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2024-04-15 21:07:03 -0300

    update axios

--- tech-news/src/lib/axios.ts
+++ tech-news/src/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001', // http://localhost:3000 substituir localhost pelo ip local
+  baseURL: 'http://192.168.0.5:3001', // http://localhost:3000 substituir localhost pelo ip local
 });
 
 export default api;
